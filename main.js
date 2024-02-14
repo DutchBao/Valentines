d6f4a0bb4a79d348093edf2c7b7c83d79c5f5fa8
DutchBao <159937186+DutchBao@users.noreply.github.com>
2024-02-14 21:02:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.getElementById("sound").play();
+document.getElementById("show").addEventListener("click", function() {
+  document.getElementById("poem").style.display = "block";
+  document.getElementById("show").style.display = "none";
+  document.getElementById("Howl").play(); 
+});
